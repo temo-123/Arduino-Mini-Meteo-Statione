2766408162d4b8e8e4d257bf4c860fd6988ac9bd
temo-123 <samsonadze.temo9@gmail.com>
2023-02-18 07:29:48 +0400

    create: Creating progect

---
+++ Sketch/tests/2)_bmp 180 + ds1302/sketch_dec11a/sketch_dec11a.ino
@@ -0,0 +1,52 @@
+#include <DS1302.h>
+#include <Wire.h> 
+#include <BMP085.h>
+
+DS1302 rtc(2, 3, 4);
+BMP085 dps = BMP085(); 
+
+long Temperature = 0, Pressure = 0;
+
+void setup()
+{
+
+  Serial.begin(9600); 
+  Wire.begin(); 
+  delay(1000);
+  dps.init();  
+  
+  rtc.halt(false);
+  rtc.writeProtect(false);
+
+  Serial.begin(9600);
+
+  rtc.setDOW(FRIDAY);        // Set Day-of-Week to FRIDAY
+  rtc.setTime(01, 33, 00);     // Set the time to 12:00:00 (24hr format)
+  rtc.setDate(11, 12, 2016);   // Set the date to August 6th, 2010
+}
+
+
+void loop(void) { 
+  dps.getPressure(&Pressure); 
+  dps.getTemperature(&Temperature);
+
+  Serial.print("Pressure(mm Hg):"); 
+  Serial.print(Pressure/133.3);      //Выводим давление в мм.рт ст
+  Serial.print("Temp:"); 
+  Serial.println(Temperature*0.1);   // Температуру в градусах Цельсия
+  delay(1000); 
+  
+  // Send Day-of-Week
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  
+  // Send date
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+
+  // Send time
+  Serial.println(rtc.getTimeStr());
+  
+  // Wait one second before repeating :)
+  delay (1000);
+}
---
+++ Sketch/tests/3)_oled 128x64/ssd1306_128x64_spi/ssd1306_128x64_spi.ino
@@ -0,0 +1,11 @@
+#include <OLED_I2C.h> 
+OLED  myOLED(8, 9, 8); // SDA - 8pin, SCL - 9pin
+void setup()
+{
+  myOLED.begin();
+}
+void loop()
+{
+  myOLED.print("hellow world !!!", CENTER, 0);
+
+}
